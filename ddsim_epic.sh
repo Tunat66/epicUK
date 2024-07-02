4126c875a4dad3dfba95126186e94b872e19f7ab
pemb7000 <pemb7000@pplxint12.physics.ox.ac.uk>
2024-07-02 18:05:31 +0100

    added some packed bash scripts

---
+++ ddsim_epic.sh
@@ -0,0 +1,2 @@
+ddsim --steeringFile steering_ee_1GeV_10GeV_epic.py --compactFile $DETECTOR_PATH/epic.xml -G --numberOfEvents 100 --outputFile ee_1GeV_10GeV_epic_1e1.edm4hep.root
+
---
+++ ddsim_epic_pythia8.sh
@@ -0,0 +1,2 @@
+ddsim --compactFile $DETECTOR_PATH/$DETECTOR_CONFIG.xml --numberOfEvents 10 --inputFiles pythia8NCDIS_10x100.hepmc --outputFile pythia8NCDIS_10x100.edm4hep.root
+
