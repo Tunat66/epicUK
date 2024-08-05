29f3d3d3a288c3d1fe2e697791d0e0ec75e23cc4
pemb7000 <pemb7000@pplxint12.physics.ox.ac.uk>
2024-08-05 15:38:26 +0100

    some new files to add cross section plots

--- analyse.sh
+++ analyse.sh
@@ -26,26 +26,26 @@ echo ${formatted_mom_array[@]}
 
 #change to the particular results dir to analyse the data
 cd $results_dir
-#mkdir -p truthseed/pi-/mom realseed/pi-/mom truthseed/pi-/dca realseed/pi-/dca
-#
-## run the analysis
-#for ((iparticle=0; iparticle<${#particle_array[@]}; iparticle++)); do
-## truth seeding
-#for ((i=0; i<${#mom_array[@]}; i++)); do
-##Form("./%s_%1.1f",mom)
-#root -b -l -q Tracking_Performances.C'("'${filename}_$(printf "%.2f" ${mom_array[i]})'.edm4eic.root","'${particle_array[iparticle]}'",'${mom_array[i]}',0.15,"")'
-#done
-#
-## real seeding
-#for ((i=0; i<${#mom_array[@]}; i++)); do
-#root -b -l -q Tracking_Performances.C'("'${filename}_$(printf "%.2f" ${mom_array[i]})'.edm4eic.root","'${particle_array[iparticle]}'",'${mom_array[i]}',0.15,"Seeded")'
-#done
-#done
-#cd truthseed/pi-/dca
-#hadd final_hist_dca_truthseed.root *.root
-#cd ../../../realseed/pi-/dca/
-#hadd final_hist_dca_realseed.root *.root
-#cd ../../../
+mkdir -p truthseed/pi-/mom realseed/pi-/mom truthseed/pi-/dca realseed/pi-/dca
+
+# run the analysis
+for ((iparticle=0; iparticle<${#particle_array[@]}; iparticle++)); do
+# truth seeding
+for ((i=0; i<${#mom_array[@]}; i++)); do
+#Form("./%s_%1.1f",mom)
+root -b -l -q Tracking_Performances.C'("'${filename}_$(printf "%.2f" ${mom_array[i]})'.edm4eic.root","'${particle_array[iparticle]}'",'${mom_array[i]}',0.15,"")'
+done
+
+# real seeding
+for ((i=0; i<${#mom_array[@]}; i++)); do
+root -b -l -q Tracking_Performances.C'("'${filename}_$(printf "%.2f" ${mom_array[i]})'.edm4eic.root","'${particle_array[iparticle]}'",'${mom_array[i]}',0.15,"Seeded")'
+done
+done
+cd truthseed/pi-/dca
+hadd final_hist_dca_truthseed.root *.root
+cd ../../../realseed/pi-/dca/
+hadd final_hist_dca_realseed.root *.root
+cd ../../../
 
 rm -rf Final_Results/ Debug_Plots/ 
 mkdir -p Final_Results/pi-/mom Final_Results/pi-/dca  Debug_Plots/truth/pi-/mom  Debug_Plots/truth/pi-/dca  Debug_Plots/real/pi-/mom Debug_Plots/real/pi-/dca
