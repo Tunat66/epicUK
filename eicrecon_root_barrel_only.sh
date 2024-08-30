e5d3e54dab1186e0c91301f0fba29c2f80ac241d
pemb7000 <pemb7000@pplxint13.physics.ox.ac.uk>
2024-08-30 15:26:43 +0100

    added some essential files before leaving :(

---
+++ eicrecon_root_barrel_only.sh
@@ -0,0 +1,2 @@
+eicrecon -Ppodio:output_collections=ReconstructedParticles -Pacts:LogLevel=trace -Pdd4hep:xml_files=epic_craterlake_tracking_only0.xml epic_test_barrel_only.root
+#-Pplugins=BVTX.so
---
+++ epicUK.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export mom_value_global=$1
+../eic-shell -- ./run_single_momentum.sh
