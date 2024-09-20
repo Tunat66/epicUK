152bc4570edb0346a7c6903a32ff178321ca7a99
Tuna Tasali <84022804+Tunat66@users.noreply.github.com>
2024-09-20 22:14:46 +0300

    Update visualize.sh

--- visualize.sh
+++ visualize.sh
@@ -2,5 +2,5 @@
 echo "Pls enter the xml file you want to visualize as the first argument of the script"
 
 dd_web_display -o $DETECTOR_PATH/detector_geometry.root --export $1
-root -l  $DETECTOR_PATH/root_visualize.C
+#root -l  $DETECTOR_PATH/root_visualize.C
 
